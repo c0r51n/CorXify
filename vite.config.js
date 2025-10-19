3dbf33c2d5b9c92189b2dee706a27ac475811fc2
c0r51n <you@example.com>
2025-10-19 20:46:02 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 🔥 wichtig! damit lädt Vite alle Assets relativ
+  base: "./", // 🔥 sehr wichtig für relative Asset-Pfade
 });
